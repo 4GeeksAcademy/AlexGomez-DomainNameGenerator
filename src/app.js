7c2370a529fba6c97c32477e45ad7b180761e984
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-10-08 02:58:29 +0000

    ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -7,14 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  function addToList(valor) {
+    var ul = document.getElementById("ul");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${valor}`));
+    ul.appendChild(li);
+  }
+
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-
+  let extension = [".com", ".net", ".us", ".io"];
   for (let index = 0; index < pronoun.length; index++) {
     for (let y = 0; y < adj.length; y++) {
       for (let x = 0; x < noun.length; x++) {
-        console.log(`${pronoun[index]}${adj[y]}${noun[x]}.com`);
+        for (let z = 0; z < extension.length; z++) {
+          addToList(`${pronoun[index]}${adj[y]}${noun[x]}${extension[z]}`);
+        }
       }
     }
   }
