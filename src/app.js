a44d0083e0e6373ed86c42f194bc6c319d60926e
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-10-07 21:29:08 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let x = 0; x < noun.length; x++) {
+        console.log(`${pronoun[index]}${adj[y]}${noun[x]}.com`);
+      }
+    }
+  }
 };
